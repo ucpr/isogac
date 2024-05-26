fbee615b3a13d1dcfad6167b36ffcf67a6c44d38
ucpr <contact@ucpr.dev>
2024-05-27 02:29:46 +0900

    feat: set busy if event contains 全休

--- index.ts
+++ index.ts
@@ -1,3 +1,47 @@
-function main() {
-  Logger.log("Hello World!");
+interface Config {
+  calendarId: string;
+  githubToken: string;
+}
+
+function newConfig(): Config {
+  const properties = PropertiesService.getScriptProperties()
+  const cid: string | null = properties.getProperty("CALENDAR_ID");
+  if (cid === null) {
+    throw new Error("CALENDAR_ID is not set");
+  }
+  const ghToken: string | null = properties.getProperty("GITHUB_TOKEN");
+  if (ghToken === null) {
+    throw new Error("GTIHUB_TOKEN is not set");
+  }
+
+  return {
+    calendarId: cid,
+    githubToken: ghToken,
+  }
+}
+
+async function main() {
+  const config = newConfig();
+  const today = new Date();
+
+  const github = new GitHubGraphQLClient(config.githubToken);
+
+  const calendar = CalendarApp.getCalendarById(config.calendarId);
+  const events = calendar.getEventsForDay(today);
+
+  let isBusy = false;
+  events.forEach(event => {
+    if (event.getTitle().includes("全休")) {
+      isBusy = true;
+      return;
+    }
+  });
+
+  if (isBusy) {
+    try {
+      await github.updateUserStatus("busy", ":love_you_gesture:", true);
+    } catch (e) {
+      Logger.log(`Error: ${e}`);
+    }
+  }
 }
